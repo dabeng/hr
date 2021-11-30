2304915ff385fc9814b2872c51e36826f469e2f5
dabeng <dabeng413@gmail.com>
2021-11-30 20:53:39 +0800

    add week header

--- src/features/calendar/MonthView.js
+++ src/features/calendar/MonthView.js
@@ -8,18 +8,35 @@ const MonthView = () => {
 
   const createCards = () => {
     let rows = [];
-    for (var i = 0; i < 7; i++) {
-      let columns = [];
-      for (var j = 0; j < 7; j++) {
-        columns.push(<div className={`column ${styles.card}`} key={j}>
-          <div className="card">
-            <div className="card-content">
-              <div className="content">
-                {7 * i + j + 1}
+    rows.push(
+      <div className="columns is-mobile">
+        {['Sun','Mon','Tue','Wed','Thu','Fri','Sat'].map((caption, i) =>
+          <div className={`column ${styles.column}`} key={i}>
+            <div className={`card ${styles.week_card}`}>
+              <div className="card-content">
+                <div className="content">
+                  {caption}
+                </div>
               </div>
             </div>
           </div>
-        </div>);
+        )}
+      </div>
+    );
+    for (let i = 0; i < 7; i++) {
+      let columns = [];
+      for (let j = 0; j < 7; j++) {
+        columns.push(
+          <div className={`column ${styles.column}`} key={j}>
+            <div className="card">
+              <div className="card-content">
+                <div className="content">
+                  {7 * i + j + 1}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
       }
       rows.push(<div className="columns is-mobile" key={i}>{columns}</div>);
     }
--- src/features/calendar/YearView.js
+++ src/features/calendar/YearView.js
@@ -7,17 +7,16 @@ import styles from "./YearView.module.scss";
 const YearView = () => {
   const createCards = () => {
     let rows = [];
-    for (var i = 0; i < 12; i++) {
+    for (let i = 0; i < 12; i++) {
       let columns = [];
-      let daysInMonth = dayjs().month(i).daysInMonth();
-      for (var j = 0; j < 32; j++) {
+      let daysInMonth = dayjs().month(i).daysInMonth(); // 获取当前年份每个月的天数
+      for (let j = 0; j < 32; j++) {
         columns.push(<div className={`column ${styles.column}`} key={j}>
           <div className={"card" + (j === 0 ? ` ${styles.month_card}` : "") + (j > daysInMonth ? ` ${styles.placeholder_card}` : "")}>
             <div className={`card-content ${styles.card_content}`}>
               <div className="content">
                 {j === 0 ? i + 1 :
-                  (j > daysInMonth ? 0 : j
-                  )
+                  (j > daysInMonth ? 0 : j)
                 }
               </div>
             </div>
