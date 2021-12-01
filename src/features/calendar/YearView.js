567f44847ba191932cf6aff8a3bff280c183de44
dabeng <dabeng413@gmail.com>
2021-12-01 10:52:38 +0800

    add previous and next buttons for year view

--- src/features/calendar/YearView.js
+++ src/features/calendar/YearView.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import dayjs from 'dayjs';
 
 import styles from "./YearView.module.scss";
 
 const YearView = () => {
+  const [increment, setIncrement] = useState(0);
+
+  const previousYear = e => {
+    setIncrement(prev => prev - 1);
+  };
+
+  const nextYear = e => {
+    setIncrement(prev => prev + 1);
+  };
+
   const createCards = () => {
     let rows = [];
     for (let i = 0; i < 12; i++) {
       let columns = [];
-      let daysInMonth = dayjs().month(i).daysInMonth(); // 获取当前年份每个月的天数
+      let daysInMonth = dayjs().add(increment, 'year').month(i).daysInMonth();
       for (let j = 0; j < 32; j++) {
         columns.push(<div className={`column ${styles.column}`} key={j}>
           <div className={"card" + (j === 0 ? ` ${styles.month_card}` : "") + (j > daysInMonth ? ` ${styles.placeholder_card}` : "")}>
@@ -33,19 +43,21 @@ const YearView = () => {
       <div className="columns is-mobile">
         <div className="column is-4">
           <div className="buttons has-addons">
-            <button className="button is-primary">
+            <button className="button is-primary" onClick={previousYear}>
               <i className="fas fa-chevron-left"></i>
             </button>
             <button className="button is-light">
               Today
             </button>
-            <button className="button is-primary">
+            <button className="button is-primary" onClick={nextYear}>
               <i className="fas fa-chevron-right"></i>
             </button>
           </div>
         </div>
         <div className="column is-8">
-          <h1 className="title is-2">{dayjs().year()}</h1>
+          <h1 className="title is-2">
+            {dayjs().add(increment, 'year').year()}
+          </h1>
         </div>
       </div>
       {createCards()}
