5db83bc2363648fec0331a3a90e670dbeee33708
dabeng <dabeng413@gmail.com>
2022-01-27 14:16:13 +0800

    add gzip compression in build process

---
+++ config-overrides.js
@@ -0,0 +1,28 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+const zlib = require("zlib");
+
+module.exports = function override(config, env) {
+  config.plugins = config.plugins.concat([
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ]);
+
+  return config;
+};
